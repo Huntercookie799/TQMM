1d0edbec21aefc20dece4f094d227cd6140eb517
IDGS-803-23001488 <agtz50538@gmail.com>
2026-09-22 01:03:18 -0600

    fix: corregir íconos de la PWA

--- vite.config.js
+++ vite.config.js
@@ -17,17 +17,17 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'kirby.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'kirby.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'kirby.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'

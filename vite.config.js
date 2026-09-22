ef7b18815a4bfb1bd92660b1e1edfb81aa8f079b
IDGS-803-23001488 <agtz50538@gmail.com>
2026-09-22 00:51:07 -0600

    feat: añadir soporte para PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['kirby.png'],
+      manifest: {
+        name: 'Flores Amarillas',
+        short_name: 'Para Eli',
+        description: 'Una sorpresa de flores amarillas para Eli',
+        theme_color: '#78d2f6',
+        background_color: '#a4e5fb',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'kirby.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'kirby.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'kirby.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
